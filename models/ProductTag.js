1780a983c0b8855680c25a23803199ce2c1f8a8b
Jaylin Gross <grossjaylin@gmail.com>
2022-04-05 08:12:55 -0400

    added psedocode

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -3,8 +3,10 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Initialize Product tag model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
+
+// set up fields and rules for Product tag model
 
 ProductTag.init(
   {
